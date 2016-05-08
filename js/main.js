ce2dd6ad458a992fcc4a6ef9cc7847a5f724d8e2
GPL <gpl@GPLdeMacBook-Pro.local>
2016-05-08 19:22:15 +0800

    Site updated: 2016-05-08 19:21:48

--- js/main.js
+++ js/main.js
@@ -107,9 +107,4 @@ require([], function (){
 		$(".article a[href]").attr("target", "_blank")
 	}
 	
-	if($(".instagram").length) {
-    require(['/js/photo.js', '/fancybox/jquery.fancybox.js', '/js/jquery.lazyload.js'], function(obj) {
-        obj.init();
-    });
-}
 });
